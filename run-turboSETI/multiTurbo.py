7d67c26de9ea3f65a89452645e25d00c8f2eacb9
Noah Franz <noahf@bls0.bl.pvt>
2021-07-15 18:15:19 -0400

    updates

--- run-turboSETI/multiTurbo.py
+++ run-turboSETI/multiTurbo.py
@@ -84,10 +84,10 @@ def splitRun(nnodes, debug, t, outDir, splicedonly, unsplicedonly, sqlTable, slo
             print(f"Running turboSETI on {len(ii)} files for cadence {fileinfo['target_name'][ii].to_numpy()[0]} on compute node: {node}")
 
             if debug:
-                cmd = ['ssh', '-o ExitOnForwardFailure=True', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --test {debug} --sqlTable {sqlTable}"]
+                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --test {debug} --sqlTable {sqlTable}"]
 
             else:
-                cmd = ['ssh', '-o ExitOnForwardFailure=True',  node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --sqlTable {sqlTable}"]
+                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --sqlTable {sqlTable}"]
 
             ssh = sp.Popen(cmd, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
             ps.append(ssh)
