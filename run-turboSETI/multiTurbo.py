75c2d70c8d043e1f07fe06969607ef7e509ff119
noahfranz13 <noahfranz13@gmail.com>
2021-07-16 13:47:05 -0700

    Reformat multiTurbo for readability

--- run-turboSETI/multiTurbo.py
+++ run-turboSETI/multiTurbo.py
@@ -5,13 +5,47 @@ import numpy as np
 import pandas as pd
 import pymysql
 
-def splitRun(nnodes, debug, t, outDir, splicedonly, unsplicedonly, sqlTable, slowdebug=False):
+def getNodes(nnodes):
+    '''
+    Get a list of compute nodes based on the number of nodes given
 
-    if t:
-        start = time.time()
+    nnodes [int] : number of nodes to run the commands on
 
-    cwd = os.getcwd()
-    varPath = '~/.bash_profile'
+    returns : list of compute nodes to run on at GBT
+    '''
+
+    # Create list of available compute nodes
+    cn = []
+    for i in range(8):
+        for k in range(10):
+
+            if i == 0:
+                node = f'blc{k}'
+            else:
+                node = f'blc{i}{k}'
+
+            if int(node[-1])<=7 and node!='blc47': # skip blc47 because it has an error
+                cn.append(node)
+
+    # Choose compute nodes starting with highest number
+    cn = np.flip(cn)
+    cn = cn[:nnodes]
+
+    print(f'Running on compute nodes {min(cn)} to {max(cn)}')
+    print(f'Writing files to {outDir}')
+    return cn
+
+def getIndex(sqlTable, splicedonly, unsplicedonly, debug=False):
+    '''
+    Get list of indexes to run turboSETI on
+
+    sqlTable [str] : name of sql table to reference in FileTracking database
+    splicedonly [bool] : if only the spliced files should be run through
+    unsplicedonly [bool] : if only the spliced files should be run through
+    debug [bool] : if True, print statements for debugging
+
+    returns : list of indexes to run turboSETI on
+    '''
 
     mysql = pymysql.connect(host=os.environ['GCP_IP'], user=os.environ['GCP_USR'],
                             password=os.environ['GCP_PASS'], database='FileTracking')
@@ -45,7 +79,7 @@ def splitRun(nnodes, debug, t, outDir, splicedonly, unsplicedonly, sqlTable, slo
         whereID = np.where(arg)[0]
         ii2D.append(whereID)
 
-    if debug or slowdebug:
+    if debug:
         print(f'indexes used: {ii2D}')
 
     # Get number of files running through
@@ -54,74 +88,36 @@ def splitRun(nnodes, debug, t, outDir, splicedonly, unsplicedonly, sqlTable, slo
         length+=len(row)
     print(f"Running turboSETI on {length} files")
 
-    # Create list of available compute nodes
-    cn = []
-    for i in range(8):
-        for k in range(10):
+    return ii2D
 
-            if i == 0:
-                node = f'blc{k}'
-            else:
-                node = f'blc{i}{k}'
+def multiCommand(nodes, commands, slowdebug=False):
+    '''
+    Run n commands on n compute nodes
 
-            if int(node[-1])<=7 and node!='blc47': # skip blc47 because it has an error
-                cn.append(node)
+    nodes [list] : list of compute nodes to run on
+    commands [list] : list of commands to run on each compute nodes, the first
+                      command will be run on the first compute node, etc.
+    slowdebug [bool] : if True, prints subprocess output as it goes
 
-    # Choose compute nodes starting with highest number
-    cn = np.flip(cn)
-    cn = cn[:nnodes]
+    returns list of subprocess Popen objects, one for each compute node
+    '''
 
-    print(f'Running on compute nodes {min(cn)} to {max(cn)}')
-    print(f'Writing files to {outDir}')
     # Run on separate compute nodes
     ps = []
-    usedcn = []
-    for ii, node in zip(ii2D, cn):
+    for cmd, node in zip(commands, cn):
 
-        if len(ii) != 0:
+        ssh = sp.Popen(cmd, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
+        ps.append(ssh)
+        if slowdebug:
+            print(ssh.stdout.readlines(), ssh.stderr.readlines())
 
-            condaenv = '/home/noahf/miniconda3/bin/activate'
-
-            print(f"Running turboSETI on {len(ii)} files for cadence {fileinfo['target_name'][ii].to_numpy()[0]} on compute node: {node}")
-
-            if debug:
-                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --test {debug} --sqlTable {sqlTable}"]
-
-            else:
-                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {t} --outdir {outDir} --sqlTable {sqlTable}"]
-
-            ssh = sp.Popen(cmd, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
-            ps.append(ssh)
-            usedcn.append(node)
-            if slowdebug:
-                print(ssh.stdout.readlines(), ssh.stderr.readlines())
-
-    try:
-        for p in ps:
-            p.communicate()
-
-    except KeyboardInterrupt:
-        for p, cn in zip(ps, usedcn):
-            exitcmd = ['ssh', cn, f"kill -9 $(pidof python3 {cwd}/wrapTurbo.py)"]
-            exitssh = sp.Popen(exitcmd, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
-
-        print('All Processes Terminated')
-
-    if t:
-        print(time.time()-start)
+    return ps
 
 def main():
     '''
     Run turboSETI in parallel across multiple compute nodes on GBT
-    Must setup environment variables with access to a mysql database called
-    FileTracking with the following columns
-
-    turboSETI : if the file has been run through turboSETI
-    filepath : path to turboSETI input file, inlcuding name
-    filename : Just the name of the file being passed into turboSETI
-    target_name : Name of target in the file passed into turboSETI
-    toi : on target of the cadence to specify the output directory
-    splice : If the file is spliced of unspliced
+    Must setup environment variables with access to a mysql database as specified
+    in README
 
     INPUT OPTIONS
     nnodes      : number of compute nodes to run on, default is
@@ -129,18 +125,9 @@ def main():
     timer       : times the run if set to true, default is true
     outdir      : output directory of turboSETI files, will consist of subdirectories
                   labelled by TOI (ON target)
+    sqlTable    : name of SQL table
     splicedonly : If True only spliced files are run through
-
-    Instead of passing in username, password, and IP of the database, make
-    environment variables of
-    GCP_IP   : IP address
-    GCP_USR  : Username
-    GCP_PASS : password
-
-    RETURNS
-    TurboSETI output files in subdirectories labelled by the ON target for each
-    cadence. These subdirectories will be stored in directory specified by outdir
-
+    unsplicedonly : If True only unspliced files are run through
     '''
 
     import argparse
@@ -155,7 +142,50 @@ def main():
     parser.add_argument('--slowdebug', type=bool, default=False)
     args = parser.parse_args()
 
-    splitRun(args.nnodes, args.debug, args.timer, args.outdir, args.splicedonly, args.unsplicedonly, args.sqlTable, slowdebug=args.slowdebug)
+    if args.timer:
+        start = time.time()
+
+    condaenv = '/home/noahf/miniconda3/bin/activate'
+    cwd = os.getcwd()
+    varPath = '~/.bash_profile'
+
+    cns = getNodes(args.nnodes)
+    ii2D = getIndex(args.sqlTable,
+                    splicedonly=args.splicedonly,
+                    unsplicedonly=args.unsplicedonly,
+                    debug=args.debug)
+
+    cmds = []
+    nodes = []
+    for node, ii in zip(cns, ii2D):
+
+        if len(ii) != 0:
+
+            print(f"Running turboSETI on {len(ii)} files on compute node: {node}")
+
+            if debug:
+                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {args.timer} --outdir {args.outdir} --test {args.debug} --sqlTable {args.sqlTable}"]
+
+            else:
+                cmd = ['ssh', node, f"source {condaenv} runTurbo ; source {varPath} ; python3 {cwd}/wrapTurbo.py --ii '{ii.tolist()}' --timer {args.timer} --outdir {args.outdir} --sqlTable {args.sqlTable}"]
+
+            cmds.append(cmd)
+            nodes.append(node)
+
+    ps = multiCommand(nodes, cmds, slowdebug=args.slowdebug)
+
+    try:
+        for p in ps:
+            p.communicate()
+    except KeyboardInterrupt:
+        for p, cn in zip(ps, usedcn):
+            exitcmd = ['ssh', cn, f"kill -9 $(pidof python3 {cwd}/wrapTurbo.py)"]
+            exitssh = sp.Popen(exitcmd, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
+
+        print('All Processes Terminated')
+
+    if args.timer:
+        print(time.time()-start)
 
 if __name__ == '__main__':
     sys.exit(main())
